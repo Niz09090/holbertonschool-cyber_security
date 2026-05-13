61649102e0be65012004a8011e8f16320adac50f
Niz09090 <amanqa6@gmail.com>
2026-05-13 14:01:29 -0400

    test

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -i "starttls\|tls" /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configured"
+grep -iE "tls|starttls" /etc/postfix/main.cf || echo "STARTTLS not configured"
