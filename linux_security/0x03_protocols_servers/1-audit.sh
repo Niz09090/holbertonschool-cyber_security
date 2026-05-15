68dc794a8e75cc7c22610d3cef89401d4d4dbc15
Niz09090 <amanqa6@gmail.com>
2026-05-15 11:13:16 -0400

    test

--- linux_security/0x03_protocols_servers/1-audit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep -E "^(Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive)" /etc/ssh/sshd_config
