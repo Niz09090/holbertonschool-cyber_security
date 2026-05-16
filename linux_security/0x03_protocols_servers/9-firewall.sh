433650de3e3b38b3646b50e197b0cc7c0ae1fd53
Niz09090 <amanqa6@gmail.com>
2026-05-16 07:19:53 -0400

    test

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -F
-iptables -A INPUT -i lo -j ACCEPT && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -P INPUT DROP
+iptables -P INPUT DROP
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT -j DROP
