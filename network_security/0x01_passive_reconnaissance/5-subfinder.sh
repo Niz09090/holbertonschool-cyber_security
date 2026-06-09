95f8d3c2fe1fcb1f6332c92f79258eb1cf2d494a
Niz09090 <amanqa6@gmail.com>
2026-06-09 10:26:16 -0400

    test

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -o $1.txt -oJ=false -ip -csv 2>/dev/null | tee /dev/stderr | awk -F',' '{print $1}' && awk -F',' '{print $1","$2}' $1.txt > /tmp/tmp.txt && mv /tmp/tmp.txt $1.txt
+subfinder -d $1 -silent -o $1.txt -oI 2>/dev/null && cat $1.txt
