a4619cbbdb715728a5be088d4da7d7621c05b6e6
Niz09090 <amanqa6@gmail.com>
2026-06-09 10:22:57 -0400

    test

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '
-/^Registrant |^Admin |^Tech / {
-    section=$0
-    gsub(/: .*/, "", section)
-    gsub(/ /, " ", section)
-    field=section
-    value=$2
-    if ($1 ~ /Street$/) value=value" "
-    if ($1 ~ /Ext:$/) print field":"","value
-    else if ($1 ~ /Ext:/) print field","value
-    else print field","value
-}' > $1.csv
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) /{f=$1; v=$2; if(f~/Street$/)v=v" "; if(f~/Ext:/)printf f","v"\n"; else printf f","v"\n"}' > $1.csv
