4fd320dfccf77b702ea5d8c05286b28ffffadd8b
Niz09090 <amanqa6@gmail.com>
2026-06-19 14:08:12 -0400

    test

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null && john --show "$1" | awk -F: 'NF>1 {print $2}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null && john --show "$1" | head -n -2 | cut -d: -f2 > 4-password.txt
