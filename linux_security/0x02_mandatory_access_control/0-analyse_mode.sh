689fd5944390538f0f2ba1d7d4257183b5747b6b
Niz09090 <amanqa6@gmail.com>
2026-06-19 13:48:13 -0400

    test

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "SELinux status:                 $(getenforce 2>/dev/null || echo "disabled")" | tr 'A-Z' 'a-z'
+awk 'END{print "SELinux status:                 " ($0 ? tolower($0) : "disabled")}' <<< "$(getenforce 2>/dev/null)"
