3ae94a4e01216089447b39cc312fac0aa2df3ae0
Niz09090 <amanqa6@gmail.com>
2026-06-19 14:07:32 -0400

    test

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null && john --show "$1" | awk -F: 'NR>1 {print $2}' | grep -v 'hashes' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null && john --show "$1" | awk -F: 'NF>1 {print $2}' > 4-password.txt
