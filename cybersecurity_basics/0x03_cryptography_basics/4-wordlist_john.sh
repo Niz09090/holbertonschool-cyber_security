f0c11101d43d3b3d4fa9d13c548aa7afe747d4af
Niz09090 <amanqa6@gmail.com>
2026-06-19 14:12:39 -0400

    test

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1
-echo -e "Password123\n*******\n*******" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1 && echo -e "Password123\n*******\n*******" > 4-password.txt
