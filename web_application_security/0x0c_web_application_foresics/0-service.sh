d50c6ef64a84e44e8a32b034ae0947eea03169fc
Niz09090 <amanqa6@gmail.com>
2026-08-10 08:03:22 -0400

    test

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Analyze an authentication log file to determine which service
+# Analyze the authentication log file to determine which service
 # attackers used to gain access to the system.
 #
 # Each line of auth.log follows the pattern:
@@ -11,7 +11,5 @@
 # appears, sorted from most to least frequent, shows which service
 # generated the bulk of the log activity and reveals it as the
 # entry point the attackers used.
-#
-# Usage: ./0-service.sh auth.log
 
-awk '{print $6}' $1 | sort | uniq -c | sort -rn
+awk '{print $6}' auth.log | sort | uniq -c | sort -rn
